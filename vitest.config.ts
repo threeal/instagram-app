5c25049e0b540b3742707c3300445db78cdbe2cc
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-19 22:25:12 +0700

    test: remove test coverage check
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,7 @@
-import { coverageConfigDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    coverage: {
-      enabled: true,
-      exclude: [...coverageConfigDefaults.exclude, "src/index.tsx"],
-      reporter: "text",
-      thresholds: { 100: true },
-    },
     browser: {
       enabled: true,
       headless: true,
