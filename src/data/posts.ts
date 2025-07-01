4ce9b301e651145b6f26172d5342e14332bb86ce
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-01 21:48:23 +0700

    DONT MERGE

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,10 +1,4 @@
-import {
-  amiraHassan,
-  budiSantoso,
-  ranggaPratama,
-  sitiNurhaliza,
-  type UserData,
-} from "./users";
+import { alfiMaulana, aniesLutfi, nurchayaSalon, type UserData } from "./users";
 
 export interface PostData {
   author: UserData;
@@ -17,107 +11,94 @@ export interface PostData {
 
 export const posts: PostData[] = [
   {
-    author: amiraHassan,
-    caption: "Hari ini cuacanya cerah banget! ☀️",
-    image:
-      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=500&h=400&fit=crop",
+    author: aniesLutfi,
+    caption:
+      "masih ga nyangka, kok kamu mau sih Alpiii 925km ke rumahku bawa keluarga, bawa bolu, bawa buah, bawa wingko, bawa dodol, bawa bunga... bawa cincin (?) kamu pengen mengembalikan darah jawa ku kah wkkw.",
+    image: "/engagement.jpg",
+    reactions: 1810,
+    date: "23 Feb 2025",
+  },
+  {
+    author: alfiMaulana,
+    caption: "hi my fiance 🥹 @Anies Lutfi",
+    image: "/cincinalfi.png",
     reactions: 3,
-    date: "15 Jun",
+    date: "24 Feb 2025",
   },
   {
-    author: budiSantoso,
-    caption: "Selamat pagi dunia!",
-    date: "2 Mar 2024",
+    author: aniesLutfi,
+    caption: "menerima undangan pertemanan @Alfi Maulana",
+    reactions: 10,
+    date: "01 Jul 2017",
   },
   {
-    author: sitiNurhaliza,
-    video:
-      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    caption: "Video lucu yang bikin ngakak! 😂",
+    author: nurchayaSalon,
+    video: "/beach.mp4",
+    caption: "Mantai dulu! 😂",
     reactions: 8,
     date: "29 Apr 2024",
   },
   {
-    author: ranggaPratama,
+    author: aniesLutfi,
+    caption: "yang main discord, ada?",
+    reactions: 19,
+    date: "18 Mar 2020",
+  },
+  {
+    author: alfiMaulana,
+    caption: "🧡 💛 💚 💙 💜",
+    image: "/lovely.png",
+    reactions: 13,
+    date: "07 Jun 2022",
+  },
+  {
+    author: aniesLutfi,
+    caption: "malu banget malu, tolong pura2 ga tahu aja 😓",
+    reactions: 190,
+    date: "09 June 2022",
+  },
+  {
+    author: alfiMaulana,
     caption:
-      "Akhirnya bisa liburan juga setelah sekian lama nggak ke luar kota. Suasana pegunungan memang nggak pernah gagal bikin hati tenang dan pikiran fresh.",
-    image:
-      "https://images.unsplash.com/photo-1519682337058-a94d519337bc?w=500&h=400&fit=crop",
-    reactions: 1,
-    date: "10 Des 2023",
+      "not wise from this far away, afraid of hurting someone, too hurts to leave. ",
+    reactions: 49,
+    date: "09 June 2022",
   },
   {
-    author: amiraHassan,
-    image:
-      "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=500&h=400&fit=crop",
-    reactions: 20,
-    date: "7 Mei",
+    author: alfiMaulana,
+    caption: "in relationship @Anies Lutfi",
+    reactions: 13,
+    date: "11 Jun 2020",
   },
   {
-    author: budiSantoso,
-    caption: "Ngopi dulu biar waras ☕️",
-    reactions: 2,
-    date: "30 Jan 2023",
+    author: alfiMaulana,
+    caption: "🌊⛵️",
+    image: "/ocean.png",
+    reactions: 97,
+    date: "20 Aug 2022",
   },
   {
-    author: sitiNurhaliza,
+    author: aniesLutfi,
     caption:
-      "Terima kasih atas semua dukungan teman-teman selama ini. Hari ini aku resmi lulus! 🎓",
-    image:
-      "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=500&h=400&fit=crop",
-    date: "21 Jul 2024",
+      "don't you say, Ginger Island or Calico Dessert -_-  @Alfi Maulana",
+    image: "/nikahmeme.png",
+    reactions: 85,
+    date: "10 Jun 2025",
   },
   {
-    author: ranggaPratama,
-    image:
-      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=500&h=400&fit=crop",
-    reactions: 5,
-    date: "28 Feb",
-  },
-  {
-    author: amiraHassan,
-    caption: "Baca buku sambil hujan itu combo terbaik. 📖🌧️",
-    date: "13 Okt 2023",
-  },
-  {
-    author: budiSantoso,
+    author: alfiMaulana,
     caption:
-      "Senja di pantai selalu memberikan rasa damai. Nggak perlu kata-kata, cukup duduk dan menikmati setiap detiknya.",
-    image:
-      "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?w=500&h=400&fit=crop",
-    reactions: 50,
-    date: "5 Sep 2024",
+      "how to be platinum member on shopee? nikah sayang 🤗 @Anies Lutfi",
+    image: "/shopee.png",
+    reactions: 85,
+    date: "10 Jun 2025",
   },
   {
-    author: sitiNurhaliza,
-    caption: "Masakan rumah memang selalu juara! 🍲",
-    reactions: 12,
-    date: "18 Agu",
-  },
-  {
-    author: ranggaPratama,
+    author: nurchayaSalon,
     caption:
-      "Proyek baru, tantangan baru! Semoga semua berjalan lancar sampai akhir.",
-    reactions: 1,
-    date: "12 Nov 2023",
-  },
-  {
-    author: amiraHassan,
-    image:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&h=400&fit=crop",
-    date: "1 Jan 2025",
-  },
-  {
-    author: budiSantoso,
-    caption: "Ayo semangat mulai minggu baru! 💪",
-    reactions: 2,
-    date: "4 Mar",
-  },
-  {
-    author: sitiNurhaliza,
-    caption: "Pelangi setelah hujan 🌈",
-    image:
-      "https://images.unsplash.com/photo-1535914254981-b5012eebbd15?w=500&h=400&fit=crop",
-    date: "22 Feb 2025",
+      "salam interaksi cantik-cantikku, kegiatan MUA di bulan suro, full smoting 😁 yang mau ngelurusin rambut, mari merapat di salon Nur Cahya. #fbpro #fyp #viral",
+    image: "/smoothing.png",
+    reactions: 196,
+    date: "28 Jun 2025",
   },
 ];
--- src/data/users.ts
+++ src/data/users.ts
@@ -3,26 +3,17 @@ export interface UserData {
   avatar: string;
 }
 
-export const amiraHassan: UserData = {
-  name: "Amira Hasan",
-  avatar:
-    "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=100&h=100&fit=crop&crop=face",
+export const alfiMaulana: UserData = {
+  name: "Alfi Maulana",
+  avatar: "/alfi.png",
 };
 
-export const budiSantoso: UserData = {
-  name: "Budi Santoso",
-  avatar:
-    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=100&h=100&fit=crop&crop=face",
+export const aniesLutfi: UserData = {
+  name: "Anies Lutfi",
+  avatar: "/anieslutfi.png",
 };
 
-export const sitiNurhaliza: UserData = {
-  name: "Siti Nurhaliza",
-  avatar:
-    "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?w=100&h=100&fit=crop&crop=face",
-};
-
-export const ranggaPratama: UserData = {
-  name: "Rangga Pratama",
-  avatar:
-    "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?w=100&h=100&fit=crop&crop=face",
+export const nurchayaSalon: UserData = {
+  name: "Nurcahya Salon",
+  avatar: "/nurcahya.png",
 };
